7e306744e593e59394fa804f31b67146347e31a0
Baimagambetov Nurbek <nurbek.ekb2z@gmail.com>
2024-04-14 21:05:41 +0500

    Added 2 tables in City.sql

--- sql/City.sql
+++ sql/City.sql
@@ -1,1 +1,25 @@
+drop table cities;
+drop table user;
 
+create table cities(
+  city_id serial8 not null,
+  city_name varchar not null,
+  index int4 not null,
+  primary key(city_id)
+);
+
+create table user(
+  user_id serial8 not null,
+  login varchar unique not null,
+  name varchar not null,
+  city int8 not null references cities(city_id),
+  primary key (user_id)
+);
+
+insert into cities(city_name, index)
+values ('Astana', 010000),
+       ('Almaty', 050000);
+
+insert into user(login, city, name)
+values ('riotname232', 1, 'Mark'),
+       ('devmagican', 2, 'Talgat');
